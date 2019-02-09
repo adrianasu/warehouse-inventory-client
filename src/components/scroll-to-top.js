498f3aba0059b8925d8eea3dd3f6ad2dd50a8c71
Adriana Suarez <agsmq@yahoo.com>
2019-02-09 15:00:19 -0600

    Fix routes margin.

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router-dom';
 // Component that scrolls to top on a route change
 class ScrollToTop extends React.Component {
     componentDidUpdate(prevProps) {
-        console.log(this.props); // no location prop
         if (this.props.location !== prevProps.location) {
             // never triggered
             window.scrollTo(0, 0);
