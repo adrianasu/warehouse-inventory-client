f63a0a564244477d76bb76b4d2adf225121b6460
Adriana Suarez <agsmq@yahoo.com>
2019-02-13 08:13:10 -0600

    Uncomment base url in config file.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-    //process.env.REACT_APP_API_BASE_URL || 
+    process.env.REACT_APP_API_BASE_URL || 
     'http://localhost:8080/api';
 
 export const PORT = process.env.REACT_APP_PORT || 8080;
